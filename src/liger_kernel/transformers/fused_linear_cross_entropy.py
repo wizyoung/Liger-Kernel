499eae44c14570c92a540f08ea84bb878ce171df
wizyoung <happyyanghehe@gmail.com>
2024-09-06 16:18:11 +0800

    (feat) add final_logit_softcap_params

--- src/liger_kernel/transformers/fused_linear_cross_entropy.py
+++ src/liger_kernel/transformers/fused_linear_cross_entropy.py
@@ -9,7 +9,7 @@ class LigerFusedLinearCrossEntropyLoss(CrossEntropyLoss):
     def __init__(self, *args, **kwargs):
         super(LigerFusedLinearCrossEntropyLoss, self).__init__(*args, **kwargs)
 
-    def forward(self, lin_weight, _input, target, bias=None):
+    def forward(self, lin_weight, _input, target, bias=None, final_logit_softcap_params=None):
         return LigerFusedLinearCrossEntropyFunction.apply(
-            _input, lin_weight, target, bias, self.ignore_index
+            _input, lin_weight, target, bias, final_logit_softcap_params, self.ignore_index
         )
